a30b7a0e2908d38613aafcef8de8e95c584fa01d
lonewolf <saurabhdalakoti07@gmail.com>
2020-12-11 22:39:25 +0530

    gradient and drawable shapes

--- app/src/main/java/com/dalakoti07/android/MainActivity.java
+++ app/src/main/java/com/dalakoti07/android/MainActivity.java
@@ -2,7 +2,11 @@ package com.dalakoti07.android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.RoundRectShape;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*RoundRectShape roundRectShape = new RoundRectShape(new float[]{
+                10, 10, 10, 10,
+                10, 10, 10, 10}, null, null);
+        ShapeDrawable shapeDrawable = new ShapeDrawable(roundRectShape);
+        shapeDrawable.getPaint().setColor(Color.parseColor("#FFFFFF"));
+        ImageView myImageView = findViewById(R.id.iv_java);
+        myImageView.setBackground(shapeDrawable);*/
+        // or you can use myImageView.setImageDrawable(shapeDrawable);
     }
 }
